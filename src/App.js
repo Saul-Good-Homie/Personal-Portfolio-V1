122f91cf2594253718c8eef5552d5d6515910186
Saul Belisle <rbelisle1270@gmail.com>
2022-05-02 15:22:24 -0500

    add navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Component } from "react";
+import NavBar from "./Components/NavBar";
+import { Routes, BrowserRouter as Router } from "react-router-dom";
+import { Route } from "react-router";
+//import NoMatchPage from "./NoMatchPage";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <NavBar />
+        {/* <div className="container-fluid">
+          <Routes>
+            <Route path="/" exact element={<Login />} />
+            <Route path="/dashboard" exact element={<Dashboard />} />
+            <Route path="/Customers" exact element={<CustomerList />} />
+            <Route path="/cart" exact element={<ShoppingCart />} />
+            <Route path="*" element={<NoMatchPage />} />
+          </Routes>
+        </div> */}
+      </Router>
+    );
+  }
 }
-
-export default App;
