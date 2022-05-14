c05babbf9172f1183a4b50fb201a8be022d1d305
Saul Belisle <rbelisle1270@gmail.com>
2022-05-13 20:06:53 -0500

    added content to projects section

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
-import MemoryGame from "../Images/React-memory-game.png";
+import MemoryGame from "../Images/React-memory-game-2.png";
+import NYT from "../Images/NYT.png";
+import ShoppingCart from "../Images/shopping-cart.png";
 
 export default class Projects extends Component {
   render() {
@@ -15,10 +17,10 @@ export default class Projects extends Component {
                     className="img-fluid"
                     src={MemoryGame}
                     alt="simple user interface for a 9x9 grid card-matching game"
-                  />
+                  ></img>
                 </div>
                 <div class="post-info">
-                  <span>June 25, 2020</span>
+                  {/* <span>June 25, 2020</span> */}
                   <h2>React Memory Card Game</h2>
                 </div>
               </div>
@@ -45,48 +47,67 @@ export default class Projects extends Component {
           <div className="post-wrap">
             <div className="post">
               <div className="post-front">
-                <div className="post-img"></div>
+                <div className="post-img">
+                  <img
+                    className="img-fluid"
+                    src={NYT}
+                    alt="simple user interface for a 9x9 grid card-matching game"
+                  />
+                </div>
                 <div class="post-info">
-                  <span>June 25, 2020</span>
-                  <h2>Productivity: Expectations vs Reality</h2>
+                  {/* <span>June 25, 2020</span> */}
+                  <h2>New York Times Clone</h2>
                 </div>
               </div>
               <div className="post-back">
                 <div class="post-except">
-                  <h2>Productivity: Expectations vs Reality</h2>
+                  <h2>New York Times Clone</h2>
                   <p>
-                    Et hinc magna voluptatum usu, cum veniam graece et. Ius ea
-                    scripserit temporibus, pri cu harum tacimates neglegentur.
-                    At adipisci incorrupte nam. Cu qui sumo appareat constituto,
-                    possit phaedrum inciderint ex usu, quis ignota cotidieque
-                    nam ea. Cum deserunt periculis ocurreret.
+                    In this HTML & CSS project, I am attempting to recreate an
+                    article from the New York Times. This project will primarily
+                    practice using CSS Grid and Flexbox.
                   </p>
                 </div>
-                <a href="#">Read More</a>
+                <a
+                  href="https://saul-good-homie.github.io/NYT-clone/NYT-clone/"
+                  target="_blank"
+                >
+                  View Online
+                </a>
               </div>
             </div>
           </div>
           <div className="post-wrap">
             <div className="post">
               <div className="post-front">
-                <div className="post-img"></div>
+                <div className="post-img">
+                  <img
+                    className="img-fluid"
+                    src={ShoppingCart}
+                    alt="simple user interface for a 9x9 grid card-matching game"
+                  />
+                </div>
                 <div class="post-info">
-                  <span>June 25, 2020</span>
-                  <h2>Productivity: Expectations vs Reality</h2>
+                  {/* <span>June 25, 2020</span> */}
+                  <h2>React Shopping Cart</h2>
                 </div>
               </div>
               <div className="post-back">
                 <div class="post-except">
-                  <h2>Productivity: Expectations vs Reality</h2>
+                  <h2>React Shopping Cart</h2>
                   <p>
-                    Et hinc magna voluptatum usu, cum veniam graece et. Ius ea
-                    scripserit temporibus, pri cu harum tacimates neglegentur.
-                    At adipisci incorrupte nam. Cu qui sumo appareat constituto,
-                    possit phaedrum inciderint ex usu, quis ignota cotidieque
-                    nam ea. Cum deserunt periculis ocurreret.
+                    In this project I use React JS to create a simple shop
+                    storefront with a functional shopping cart. The purpose of
+                    this project is to practice managing state, page routing,
+                    switch and links.
                   </p>
                 </div>
-                <a href="#">Read More</a>
+                <a
+                  href="https://saul-good-homie.github.io/react-shopping-cart/#/"
+                  target="_blank"
+                >
+                  View Online
+                </a>
               </div>
             </div>
           </div>
